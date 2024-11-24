ea59163dd8969c23d477f962e8399badb255f694
ruizhong123 <145686473+ruizhong123@users.noreply.github.com>
2024-11-24 22:40:14 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,20 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 
-data = pd.read_excel("C:\\資料探勘\\website_create\\房價113(1).xlsx")
+import mysql.connector as con 
+import pandas as pd
+
+columns = ['鄉鎮市區', '交易標的', '土地位置建物門牌', 'Latitude', 'Longitude', '都市土地使用分區', '交易年月日', '交易筆棟數', '移轉層次', '總樓層數', '建物型態', '主要用途', '主要建材', '建物現況格局.房', '建物現況格局.廳', '建物現況格局.衛', '建物現況格局.隔間', '有無管理組織', '車位類別', '電梯', 'm', 'n', '單價(萬/坪)', '土地移轉總面積(坪).1', '建物移轉總面積(坪)', '車位移轉總面積(坪)', '車位總價元(萬)', '總價元(萬)', '主建物面積(坪)', '附屬建物面積(坪)', '陽台面積(坪)', '屋齡', '是否為預購屋', '是否夠買一層以上的房子', '購買是否有買夾層屋', '公設面積包含電梯或樓梯', '公設面積包含屋頂突出物', '附屬面積含平台', '附屬面積含露台', '購買時是否有騎樓', '是否有購地下樓層', '最低地上樓層', '主建物占比', '陽台面積占比', '附屬面積占比', '公佔比', '是否有記附屬面積', '備註']
+
+conn = con.connect(host = '127.0.0.1',port =  3306,password = '123456',user = 'root',database = 'house')
+cur = conn.cursor()
+
+cur.execute("""select*from house""")
+data = cur.fetchall()
+
+data = pd.DataFrame(data,columns=columns)
+
+
 data = data.dropna()
 st.set_page_config(layout="wide")
 st.title("不動產交易房屋資訊")
